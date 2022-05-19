8f1842954666f4e81c3a5fb79a1be793d1b23779
Michael Cortez-Mejia <bayarea6fifty@gmail.com>
2022-05-19 15:39:22 -0700

    \initial set up and push

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// ReactDom library is rendering tehj App component at the root element in the HTML 
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
